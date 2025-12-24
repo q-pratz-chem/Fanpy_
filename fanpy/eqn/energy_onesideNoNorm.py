b324fa45577f171a99bc10be58a95b23bbf0283f
Pratiksha Gaikwad <p.gaikwad@c0709a-s30.ufhpc>
2025-12-24 11:41:35 -0500

    Add EnergyOneSideProjectionNoNorm objective
    - Done to avoid repeated internal call from BFGS to objective and
      gradient causing repeated computations of overlaps and derivatives
      in a single iteraton of optimization of parameters
    - based on EnergyOneSideProjection
    - with objective: assign_params=True, normalize=False, save=True
    - with gradient: assign_params=False, normalize=False, save=True

---
+++ fanpy/eqn/energy_onesideNoNorm.py
@@ -0,0 +1,11 @@
+from .energy_oneside import EnergyOneSideProjection
+
+
+class EnergyOneSideProjectionNoNorm(EnergyOneSideProjection):
+    def objective(self, params, assign=True, normalize=False, save=True):
+        # always disable normalization during optimization
+        return super().objective(params, assign=assign, normalize=False, save=save)
+
+    def gradient(self, params, assign=False, normalize=False, save=True):
+        # also disable normalization for gradient calls
+        return super().gradient(params, assign=assign, normalize=False, save=save)
